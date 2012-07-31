4c2c83ed626b65e0c37ae61e0919892820ecb038
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2012-07-31 23:09:38 +0200

    added empty service file

--- lib/Provision/DSL/Entity/Service.pm
+++ lib/Provision/DSL/Entity/Service.pm
@@ -3,4 +3,11 @@ use Moo;
 
 extends 'Provision::DSL::Entity::File';
 
+#
+# /etc/init.d --> service
+# /etc/rcX.d  --> je nach runlevel
+#
+# evtl. schon vorhanden.
+# /sbin/start-stop-daemon verwenden!
+
 1;
