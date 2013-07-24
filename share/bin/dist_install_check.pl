47f776a937c0241b6b9f07f5ff7ef65b68a7a913
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2013-07-24 16:43:32 +0200

    modified is_missing check for modules

--- share/bin/dist_install_check.pl
+++ share/bin/dist_install_check.pl
@@ -4,15 +4,27 @@ use warnings;
 
 my @missing_modules;
 my @outdated_modules;
+my $i;
+
+sub is_missing {
+    local $" = ' ';
+    $i++;
+
+    # package needed because Moose* classes apply a meta class
+    # and this should only happen once per package
+    eval "package x$i; use @_; 1";
+
+    return $@;
+}
 
 while (<>) {
     s{\A\s+|\s+\z}{}xmsg;
     my ($module, $version) = split '~';
     next if !$module;
     
-    if (!eval "use $module; 1") {
+    if (is_missing $module) {
         push @missing_modules, $module;
-    } elsif ($version && !eval "use $module $version; 1") {
+    } elsif ($version && is_missing $module, $version) {
         $@ =~ s{\sat\s.*}{}xms;
         push @outdated_modules, $@;
     }
@@ -21,5 +33,5 @@ while (<>) {
 exit if !@missing_modules && !@outdated_modules;
 
 print STDERR "Missing: $_\n" for @missing_modules;
-print STDERR "$_\n" for @outdated_modules;
+print STDERR "$_\n"          for @outdated_modules;
 exit 1;
